067776f7c9f6635c19844fc7eb44e59e301ee1ad
SunArth Systems <sunarthsystems@gmail.com>
2026-07-30 17:21:19 +0530

    Add Prawaas 2026 blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://blog.sunarth.com',
   integrations: [sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
